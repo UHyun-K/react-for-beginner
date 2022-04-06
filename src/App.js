f1adda8b2448f61c38473f15085d4b317be7197f
UHyun-K <konglim2@naver.com>
2022-04-06 14:13:34 +0900

    to Do List

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import { useState, useEffect } from "react";
-function Hello() {
-    useEffect(() => {
-        console.log("hi");
-        return () => console.log("bye");
-    }, []);
-    return <h1>Hello</h1>;
-}
+import { useState } from "react";
+
 function App() {
-    const [showing, setShowing] = useState(false);
-
-    const onClick = () => setShowing((prev) => !prev);
-
-    useEffect(() => console.log("call the api"), []);
-
-    console.log("i run all the time");
-
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return; //to Do가 없으면 이 함수가 동작하지 않도록
+        }
+        setToDo(""); // submit후 input에 value값 삭제  초기화
+        setToDos((currentArray) => {
+            const newArray = [toDo, ...currentArray];
+            console.log(newArray);
+            return newArray;
+        });
+        console.log(toDos);
+    };
+    const onClick = (index) => {
+        setToDos(toDos.filter((item, toDoIndex) => index !== toDoIndex));
+    };
     return (
         <div>
-            {showing ? <Hello /> : null}
-            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+            <h1>My To Do List({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    value={toDo}
+                    type="text"
+                    placeholder="Write your to do..."
+                />
+                <button>Add To Do</button>
+            </form>
+            <hr />
+            <ul>
+                {toDos.map((item, index) => (
+                    <li key={index}>
+                        {item}
+                        <button onClick={() => onClick(index)}>❌</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
