2f0f56cbd51f2b3935bf57014cb7106c47b65ea7
UHyun-K <konglim2@naver.com>
2022-04-06 14:59:48 +0900

    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,46 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-    const [toDo, setToDo] = useState("");
-    const [toDos, setToDos] = useState([]);
-    const onChange = (event) => setToDo(event.target.value);
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if (toDo === "") {
-            return; //to Do가 없으면 이 함수가 동작하지 않도록
-        }
-        setToDo(""); // submit후 input에 value값 삭제  초기화
-        setToDos((currentArray) => {
-            const newArray = [toDo, ...currentArray];
-            console.log(newArray);
-            return newArray;
-        });
-        console.log(toDos);
-    };
-    const onClick = (index) => {
-        setToDos(toDos.filter((item, toDoIndex) => index !== toDoIndex));
-    };
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers?limit=100")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json);
+                setLoading(false);
+            });
+    }, []);
     return (
         <div>
-            <h1>My To Do List({toDos.length})</h1>
-            <form onSubmit={onSubmit}>
-                <input
-                    onChange={onChange}
-                    value={toDo}
-                    type="text"
-                    placeholder="Write your to do..."
-                />
-                <button>Add To Do</button>
-            </form>
-            <hr />
-            <ul>
-                {toDos.map((item, index) => (
-                    <li key={index}>
-                        {item}
-                        <button onClick={() => onClick(index)}>❌</button>
-                    </li>
-                ))}
-            </ul>
+            <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+            {loading ? (
+                <strong>Loading..</strong>
+            ) : (
+                <select>
+                    {coins.map((coin) => (
+                        <option>
+                            {coin.name}({coin.symbol}):{coin.quotes.USD.price}{" "}
+                            USD
+                        </option>
+                    ))}
+                </select>
+            )}
         </div>
     );
 }
