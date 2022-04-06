dde883ce0f4dfdf304644e4f0fafc83e0a2655b3
UHyun-K <konglim2@naver.com>
2022-04-06 22:58:03 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import { useState, useEffect } from "react";
-import Movie from "./components/Movie";
+import Home from "./routes/Home";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+// import your route components too
 
 function App() {
-    return null;
+    return (
+        <Router>
+            <Switch>
+                <Route path="/movie">
+                    <Detail />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
