8b2f6dda19ccd5198c4ea717d98c43b9d0ad2e17
UHyun-K <konglim2@naver.com>
2022-04-11 14:42:38 +0900

    publishing, deploy

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import Detail from "./routes/Detail";
 
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/movie/:id">
-                    <Detail />
-                </Route>
-                <Route path="/">
-                    <Home />
-                </Route>
-            </Switch>
-        </Router>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/movie/:id" element={<Detail />}></Route>
+                <Route
+                    path={`${process.env.PUBLIC_URL}/`}
+                    element={<Home />}
+                ></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 export default App;
