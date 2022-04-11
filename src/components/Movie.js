66e3a5c405bba3d21441c6430881cb6cb2b7141f
UHyun-K <konglim2@naver.com>
2022-04-11 15:29:22 +0900

    styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,17 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "../Movie.module.css"
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, year, summary,  genres }) {
     return (
-        <div>
-            <img src={coverImg} alt={title} />
-            <h2>
+        <div className={styles.movie}>
+            <img src={coverImg} alt={title}  className={styles.movie_img}/>
+            <h2 className={styles.movie__title}>
                 <Link to={`/movie/${id}`}>{title}</Link>
             </h2>
-            <p>{summary}</p>
-            <ul>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <ul className={styles.movie__genres}>
                 {genres == null ? "" : genres.map((g) => <li key={g}>{g}</li>)}
             </ul>
         </div>
@@ -21,6 +23,7 @@ Movie.prototype = {
     coverImg: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
+    year:PropTypes.number.isRequired,
     genres: PropTypes.arrayOf(PropTypes.String),
 };
 
